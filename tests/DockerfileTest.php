25311f800ed420f454783b759cddc744419d9797
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-07-03 21:02:13 -0400

    Update DockerfileTest.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- tests/DockerfileTest.php
+++ tests/DockerfileTest.php
@@ -19,7 +19,7 @@ class DockerfileTest extends TestCase
         $output = shell_exec("docker build -t humhub-docker -f $dockerfilePath . 2>&1");
 
         // Check if image build was successful
-        $this->assertStringContainsString('Successfully built', $output, 'Docker build failed: ' . $output);
+        $this->assertStringContainsString('DONE', $output, 'Docker build failed: ' . $output);
 
         // Clean up: remove test image
         $cleanupOutput = shell_exec("docker rmi humhub-docker 2>&1");
